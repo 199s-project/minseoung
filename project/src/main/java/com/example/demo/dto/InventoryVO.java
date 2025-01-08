dad3356588f30342b32af3099a74eb01f191f10a
MinseoungKim1 <alstjd9508@gmail.com>
2025-01-08 12:14:27 +0900

    김민성 github 최신화

--- project/src/main/java/com/example/demo/dto/InventoryVO.java
+++ project/src/main/java/com/example/demo/dto/InventoryVO.java
@@ -9,11 +9,9 @@ import lombok.Setter;
 @Setter
 @Alias("InventoryVO")
 public class InventoryVO {
-	
-	public int inventory_num;
-	public String inventory_name;
-	public int inventory_price;
-	public int inventory_amount;
-	public String inventory_regdate;
-	
+	private int inventory_num;
+	private String inventory_name;
+	private int inventory_price;
+	private int inventory_amount;
+	private int inventory_regdate;
 }
